a8b7b527fdbc837da808066bd04a961df75925ce
Carl <hovlandaexpress1@gmail.com>
2026-04-22 10:24:28 +0200

    grid

--- minesweep.js
+++ minesweep.js
@@ -7,3 +7,18 @@ console.log("carl");
 for (let i = 0; i < 1000; i++) {
     console.log("Carl Åhs, Hovlandaexpress@gmail.com");
   }
+//person 1 (carl)
+  const width = 10;
+const height = 10;
+
+let board = [];
+
+for (let y = 0; y < height; y++) {
+  let row = [];
+  for (let x = 0; x < width; x++) {
+    row.push({
+      isMine: false,
+    });
+  }
+  board.push(row);
+}
